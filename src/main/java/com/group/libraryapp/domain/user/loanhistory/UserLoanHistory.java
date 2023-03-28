5eb1eda4fae20d58726467fef349fa78aa2b3199
Jaeyeon Cho <cjyeon1022@gmail.com>
2023-03-28 15:59:39 +0900

    fix : Upgrade Spring Boot 3.0.x

--- src/main/java/com/group/libraryapp/domain/user/loanhistory/UserLoanHistory.java
+++ src/main/java/com/group/libraryapp/domain/user/loanhistory/UserLoanHistory.java
@@ -1,14 +1,13 @@
 package com.group.libraryapp.domain.user.loanhistory;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-
 import com.group.libraryapp.domain.user.User;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
